dfda1410931d187cac3352353c91ec740459d261
Nenad Novakovic <dvlden@users.noreply.github.com>
2023-02-02 12:13:48 +0100

    test: separate tests into different files

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,8 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { createMultiloginApp } from './index'
-import { ProfileInsight } from './types'
 
-// TODO: Maybe mock entire API instead of actually using it?
-// It's bad that we have to use index method on many tests
-// As there is no-content in response of update and destroy
 describe('index.ts', () => {
   const app = createMultiloginApp()
-  let profile: ProfileInsight
 
   it('exports a function', () => {
     expect(typeof createMultiloginApp).toBe('function')
@@ -15,59 +10,5 @@ describe('index.ts', () => {
 
   it('returns an object with profile property', () => {
     expect(typeof app.profile).toEqual('object')
-  })
-
-  it('creates a profile', async () => {
-    const create = vi.fn(app.profile.create)
-
-    profile = (await create({
-      name: 'Biggie Smalls',
-      os: 'mac',
-      browser: 'mimic',
-    })) as ProfileInsight
-
-    expect(create).toHaveBeenCalled()
-    expect(profile).toHaveProperty('uuid')
-  })
-
-  it('updates the profile', async () => {
-    const index = vi.fn(app.profile.index)
-    const update = vi.fn(app.profile.update)
-
-    await update(profile.uuid, {
-      name: 'The Notorious B.I.G.',
-    })
-
-    const profiles = (await index()) as ProfileInsight[]
-    const updatedProfile = profiles.find((p) => p.uuid === profile.uuid)
-
-    expect(update).toHaveBeenCalled()
-    expect(updatedProfile).toContain({
-      name: 'The Notorious B.I.G.',
-    })
-  })
-
-  it('deletes the profile', async () => {
-    const index = vi.fn(app.profile.index)
-    const destroy = vi.fn(app.profile.destroy)
-
-    await destroy(profile.uuid)
-
-    const profiles = (await index()) as ProfileInsight[]
-    const deletedProfile = profiles.find((p) => p.uuid === profile.uuid)
-
-    expect(destroy).toHaveBeenCalled()
-    expect(deletedProfile).toBeUndefined()
-  })
-
-  it('lists all profiles', async () => {
-    const index = vi.fn(app.profile.index)
-    const profiles = (await index()) as ProfileInsight[]
-
-    const deletedProfile = profiles.find((p) => p.uuid === profile.uuid)
-
-    expect(index).toHaveBeenCalled()
-    expect(profiles).toBeInstanceOf(Array)
-    expect(deletedProfile).toBeUndefined()
   })
 })
